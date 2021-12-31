8ea1fb73b8c740f76d45846cceed9325d5096c96
Jeremy Lumley <jeremy.lumley96@gmail.com>
2021-12-30 21:40:42 -0500

    continuing work on targets

--- app/giraffe_budget/time_utils.py
+++ app/giraffe_budget/time_utils.py
@@ -1,6 +1,11 @@
-import datetime
 import time
+
+from datetime import datetime
 
 
 def datestr_to_timestamp(datestring):
-    return time.mktime(datetime.datetime.strptime(datestring, "%d-%m-%Y").timetuple())
+    return time.mktime(datetime.strptime(datestring, "%d-%m-%Y").timetuple())
+
+
+def diff_month(d1, d2):
+    return (d1.year - d2.year) * 12 + d1.month - d2.month
