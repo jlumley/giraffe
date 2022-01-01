6df0000cb879f903d8c6062b79ce24dcf7b49e4c
Jeremy Lumley <jeremy.lumley96@gmail.com>
2021-12-31 20:38:13 -0500

    starting to refactor requests

--- app/giraffe_budget/blueprints/transaction.py
+++ app/giraffe_budget/blueprints/transaction.py
@@ -1,4 +1,5 @@
 import time
+import sqlite3
 
 from flask import Blueprint, current_app, request, make_response, g, jsonify
 from flask_expects_json import expects_json
@@ -23,7 +24,7 @@ transaction = Blueprint("transaction", __name__, url_prefix="/transaction")
         "reconciled": None,
     },
 )
-def get_transactions(accounts, categories, payees, before, after, cleared, reconciled):
+def _get_transactions(accounts, categories, payees, before, after, cleared, reconciled):
     """Get all transactions"""
     accounts = request.args.get("accounts", accounts)
     categories = request.args.get("categories", categories)
@@ -33,78 +34,46 @@ def get_transactions(accounts, categories, payees, before, after, cleared, recon
     cleared = request.args.get("cleared", cleared)
     reconciled = request.args.get("reconciled", reconciled)
 
-    query_statement = GET_ALL_TRANSACTIONS_STATEMENT
-    query_vars = tuple()
+    if accounts:
+        accounts = accounts.split(",")
+    if categories:
+        categories = categories.split(",")
+    if payees:
+        payees = payees.split(",")
 
-    # build query statement
-    if accounts is not None:
-        query_statement += " AND account_id IN (%s)" % ",".join(
-            "?" * len(accounts.split(","))
-        )
-        query_vars += tuple(accounts.split(","))
+    transactions = get_transactions(
+        accounts,
+        categories,
+        payees,
+        time_utils.datestr_to_timestamp(before),
+        time_utils.datestr_to_timestamp(after),
+        cleared,
+        reconciled,
+    )
 
-    if categories is not None:
-        query_statement += (
-            " AND transaction_id IN (SELECT transaction_id from"
-            "transaction_categories WHERE category_id IN (%s))" % ","
-        ).join("?" * len(categories.split(",")))
-        query_vars += tuple(categories.split(","))
-
-    if payees is not None:
-        query_statement += " AND payee_id IN (%s)" % ",".join(
-            "?" * len(payees.split(","))
-        )
-        query_vars += tuple(payees.split(","))
-
-    if before is not None:
-        query_statement += " AND date < ?"
-        query_vars += (time_utils.datestr_to_timestamp(before),)
-
-    if after is not None:
-        query_statement += " AND date > ?"
-        query_vars += (time_utils.datestr_to_timestamp(after),)
-
-    if cleared is not None:
-        query_statement += " AND cleared = ?"
-        query_vars += (to_sqlite_bool(cleared),)
-
-    if reconciled is not None:
-        query_statement += " AND reconciled = ?"
-        query_vars += (to_sqlite_bool(reconciled),)
-
-    transactions = db_utils.execute(query_statement, query_vars)
     return make_response(jsonify(transactions), 200)
 
 
 @transaction.route("/create", methods=("POST",))
 @expects_json(POST_TRANSACTION_CREATE_SCHEMA)
-def create_transaction():
+def _create_transaction():
     """Create new transaction"""
     data = request.get_json()
-    date = time_utils.datestr_to_timestamp(data["date"])
-    insert_data = {
-        "account_id": data.get("account_id"),
-        "payee_id": data.get("payee_id"),
-        "date": date,
-        "memo": data.get("memo"),
-        "cleared": int(data.get("cleared")),
-        "amount": data.get("amount"),
-    }
-    transaction = db_utils.execute(POST_TRANSACTION_CREATE_STATEMENT, insert_data)
-    if "categories" in data.keys():
-        for c in data["categories"]:
-            db_utils.execute(
-                POST_TRANSACTION_CATEGORIES_STATEMENT,
-                {
-                    "transaction_id": transaction[0]["id"],
-                    "category_id": c["category_id"],
-                    "amount": c["amount"],
-                },
-            )
-    transaction = db_utils.execute(
-        GET_TRANSACTION_STATEMENT, {"transaction_id": transaction[0]["id"]}, commit=True
-    )
-    return make_response(jsonify(transaction[0]), 201)
+
+    try:
+        transaction_id = create_transaction(
+            data.get("account_id"),
+            data.get("amount"),
+            time_utils.datestr_to_timestamp(data.get("date")),
+            int(data.get("cleared")),
+            payee_id=data.get("payee_id"),
+            memo=data.get("memo"),
+            categories=data.get("categories", []),
+        )
+    except Exception as e:
+        return make_response(jsonify(e), 400)
+
+    return make_response(jsonify({"id": transaction_id}), 201)
 
 
 @transaction.route("/update/<transaction_id>", methods=("PUT",))
@@ -112,7 +81,7 @@ def create_transaction():
 def update_transaction(transaction_id):
     """Update transaction"""
     data = request.get_json()
-    update_statement = PUT_TRANSACTION_UPDATE_STATEMENT
+    update_statement = UPDATE_TRANSACTION
     update_vars = tuple()
     if "account_id" in data.keys():
         update_statement += ", account_id = ?"
@@ -139,11 +108,11 @@ def update_transaction(transaction_id):
     # update categories
     if "categories" in data.keys():
         db_utils.execute(
-            DELETE_TRANSACTION_CATEGORIES_STATEMENT, {"transaction_id": transaction_id}
+            DELETE_TRANSACTION_CATEGORIES, {"transaction_id": transaction_id}
         )
         for c in data["categories"]:
             db_utils.execute(
-                POST_TRANSACTION_CATEGORIES_STATEMENT,
+                POST_TRANSACTION_CATEGORIES,
                 {
                     "transaction_id": transaction[0]["id"],
                     "category_id": c["category_id"],
@@ -152,26 +121,169 @@ def update_transaction(transaction_id):
             )
 
     transaction = db_utils.execute(
-        GET_TRANSACTION_STATEMENT, {"transaction_id": transaction_id}, commit=True
+        GET_TRANSACTION, {"transaction_id": transaction_id}, commit=True
     )
     return make_response(jsonify(transaction[0]), 200)
 
 
 @transaction.route("/delete/<transaction_id>", methods=("DELETE",))
-def delete_transaction(transaction_id):
+def _delete_transaction(transaction_id):
     """Delete transaction"""
-    db_utils.execute(
-        DELETE_TRANSACTION_CATEGORIES_STATEMENT, {"transaction_id": transaction_id}
+    _deleted_id = delete_transaction(transaction_id)
+    return make_response(jsonify({"id": _deleted_id}), 200)
+
+
+def delete_transaction(transaction_id):
+    """Delete a Transaction
+
+    Args:
+        transaction_id (int): transaction id
+
+    Returns:
+        int: id of deleted transaction
+    """
+    db_utils.execute(DELETE_TRANSACTION_CATEGORIES, {"transaction_id": transaction_id})
+    transaction_id = db_utils.execute(
+        DELETE_TRANSACTION, {"transaction_id": transaction_id}, commit=True
     )
-    db_utils.execute(
-        DELETE_TRANSACTION_STATEMENT, {"transaction_id": transaction_id}, commit=True
+
+    return transaction_id[0]["id"]
+
+
+def create_transaction(
+    account_id, amount, date, cleared, payee_id=None, memo=None, categories=[]
+):
+    """Create a new Transaction
+
+    Args:
+        account_id (int): account_id
+        amount (int): total transaction amount in cents
+        date (str): date of the transaction (YYY-MM-DD)
+        cleared (int): whether or not the transaction has cleared
+        payee_id (int, optional): payee_id. Defaults to None.
+        memo (str, optional): transaction description/memos. Defaults to None.
+        categories (list, optional): categories for split transactions. Defaults to [].
+
+    Raises:
+        RuntimeError: If unable to create transaction
+
+    Returns:
+        int: trnasaction id
+    """
+    transaction = db_utils.execute(
+        CREATE_TRANSACTION,
+        {
+            "account_id": account_id,
+            "amount": amount,
+            "date": date,
+            "cleared": cleared,
+            "payee_id": payee_id,
+            "memo": memo,
+        },
     )
-    return make_response(jsonify("success"), 200)
+    for c in categories:
+        db_utils.execute(
+            CREATE_TRANSACTION_CATEGORIES,
+            {
+                "transaction_id": transaction[0]["id"],
+                "category_id": c["category_id"],
+                "amount": c["amount"],
+            },
+        )
+    transaction = db_utils.execute(
+        GET_TRANSACTION, {"transaction_id": transaction[0]["id"]}, commit=True
+    )
+
+    return transaction[0]["id"]
+
+
+def get_transactions(accounts, categories, payees, before, after, cleared, reconciled):
+    """Query All transactions
+
+    Args:
+        accounts (list): list of account ids
+        categories (list): [description]
+        payees ([type]): [description]
+        before ([type]): [description]
+        after ([type]): [description]
+        cleared ([type]): [description]
+        reconciled ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    query = GET_ALL_TRANSACTIONS
+    query_vars = tuple()
+    if accounts:
+        query += f" AND account_id IN ({','.join('?' * len(accounts))})"
+        query_vars += tuple(accounts)
+
+    if categories:
+        query += (
+            f" AND transaction_id IN (SELECT transaction_id from"
+            f" transaction_categories WHERE category_id IN"
+            f" ({','.join('?' * len(categories))})"
+        )
+        query_vars += tuple(categories)
+
+    if payees:
+        query += f" AND payee_id IN ({','.join('?' * len(payees))})"
+        query_vars += tuple(payees)
+
+    if before:
+        query += " AND date < ?"
+        query_vars += (before,)
+
+    if after:
+        query += " AND date > ?"
+        query_vars += (after,)
+
+    if cleared is not None:
+        query += " AND cleared = ?"
+        query_vars += (to_sqlite_bool(cleared),)
+
+    if reconciled is not None:
+        query += " AND reconciled = ?"
+        query_vars += (to_sqlite_bool(reconciled),)
+
+    transaction_ids = db_utils.execute(query, query_vars)
+    # fetching transaction data
+    transactions = []
+    for t in transaction_ids:
+        transactions.append(get_transaction(t["id"]))
+
+    return transactions
+
+
+def get_transaction(transaction_id):
+    """Get a transaction by transaction_id
+
+    Args:
+        transaction_id (int): transaction_id
+
+    Returns:
+        dict: transaction dict
+    """
+
+    transaction = db_utils.execute(GET_TRANSACTION, {"transaction_id": transaction_id})
+    categories = db_utils.execute(
+        GET_TRANSACTION_CATEGORIES, {"transaction_id": transaction_id}
+    )
+    transaction = transaction[0]
+    transaction["categories"] = categories
+    transaction["date"] = time_utils.timestamp_to_datestr(transaction["date"])
+    return transaction
 
 
 def to_sqlite_bool(value):
-    """quick and dirty function to handle
-    boolean request params
+    """Convert string value to int boolean
+
+    Args:
+        value (str): string value to convert
+
+    Returns:
+        int: boolean value as an int
     """
     if value.lower() in ("t", "true", "yes", "y"):
         return 1
--- app/giraffe_budget/sql/transaction_statements.py
+++ app/giraffe_budget/sql/transaction_statements.py
@@ -1,35 +1,39 @@
-GET_ALL_TRANSACTIONS_STATEMENT = """SELECT *
+GET_ALL_TRANSACTIONS = """SELECT id
 FROM transactions
 WHERE True
 """
 
 
-GET_TRANSACTION_STATEMENT = """SELECT *
+GET_TRANSACTION = """SELECT *
 FROM transactions
 WHERE id = :transaction_id;
 """
 
+GET_TRANSACTION_CATEGORIES = """SELECT *
+FROM transaction_categories
+WHERE transaction_id = :transaction_id;
+"""
 
-POST_TRANSACTION_CREATE_STATEMENT = """INSERT INTO transactions
+CREATE_TRANSACTION = """INSERT INTO transactions
 (account_id, payee_id, date, memo, cleared, reconciled, amount)
-VALUES (:account_id, :payee_id, :date, :memo,
-:cleared, 0, :amount)
+VALUES (:account_id, :payee_id, :date, :memo, :cleared, 0, :amount)
 RETURNING id;
 """
 
-POST_TRANSACTION_CATEGORIES_STATEMENT = """INSERT INTO transaction_categories
+CREATE_TRANSACTION_CATEGORIES = """INSERT INTO transaction_categories
 (transaction_id, category_id, amount)
 VALUES (:transaction_id, :category_id, :amount);
 """
 
-DELETE_TRANSACTION_CATEGORIES_STATEMENT = """DELETE FROM transaction_categories
+DELETE_TRANSACTION_CATEGORIES = """DELETE FROM transaction_categories
 WHERE transaction_id = :transaction_id;
 """
 
-PUT_TRANSACTION_UPDATE_STATEMENT = """UPDATE transactions
+UPDATE_TRANSACTION = """UPDATE transactions
 SET id = id
 """
 
-DELETE_TRANSACTION_STATEMENT = """DELETE FROM transactions
-WHERE id = :transaction_id;
+DELETE_TRANSACTION = """DELETE FROM transactions
+WHERE id = :transaction_id
+RETURNING id;
 """
