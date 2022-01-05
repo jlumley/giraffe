9f1ef62bc58f30cc430b67b918f9079b79899fe5
Jeremy Lumley <jeremy.lumley96@gmail.com>
2022-01-04 19:58:07 -0500

    dynamically create category components

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import React, { useState, useEffect} from 'react'
+import instance from './axois';
 import Category from './Category';
 
+import categoryRequests from './requests/category';
+
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+      async function fetchData(){
+        // get today's date YYYY-MM-DD
+        const today = new Date().toISOString().slice(0, 10);
+        const categories = await instance.get(`${categoryRequests.fetchAllCategories}/${today}`);
+        console.log(categories)
+        setCategories(categories.data)
+      }
+      fetchData()
+  }, []);
+
+  console.log(categories)
   return (
     <div className="App">
-      <Category categoryId="1" />
-      <Category categoryId="2" />
+      {
+        categories.map(cat => {
+          return <Category category={ cat } />
+        })
+      }
     </div>
   );
 }
--- app/frontend/src/Category.js
+++ app/frontend/src/Category.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect} from 'react'
 import instance from './axois';
-import categoryRequests from './requests/category';
 import transactionRequests from './requests/transaction';
 
-function Category({categoryId}) {
-    const [category, setCategory] = useState([]);
+function Category({category}) {
     const [transactions, setTransactions] = useState([]);
 
     useEffect(() => {
@@ -12,24 +10,19 @@ function Category({categoryId}) {
             // get today's date YYYY-MM-DD
             const today = new Date().toISOString().slice(0, 10);
             const month_start = `${new Date().toISOString().slice(0, 8)}01`
-            // get category data
-            const category = await instance.get(`${categoryRequests.fetchCategory}/${categoryId}/${today}`);
             // get all transactions for this category
             const params = {
-                categories: categoryId,
+                categories: category.id,
                 after: month_start,
-                before: today,
-                limit: 500
+                before: today
             };
             const transactions = await instance.get(transactionRequests.fetchTransactions, {params});
-            setCategory(category.data[0])
             // parse transactions
             var parsed_transactions = [];
-            console.log(transactions);
             transactions.data.forEach(t => {
                 var amount = 0;
                 t.categories.forEach(c => {
-                    if (c.category_id == categoryId){
+                    if (c.category_id == category.id){
 
                         amount += c.amount;
                     }
@@ -46,11 +39,11 @@ function Category({categoryId}) {
 
         }
         fetchData()
-    }, [categoryId]);
+    }, [category]);
 
     console.log(category);
     console.log(transactions);
-    console.log(transactions.reduce((a, b) => a + b.amount , 0 ));
+
     return (
         <div>
             {/**category name */}
