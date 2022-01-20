a4b94689c6c92b68f537ec61a11a1699734b67a8
Jeremy Lumley <jeremy.lumley96@gmail.com>
2022-01-20 00:17:51 -0500

    adding category group componentswq

---
+++ app/frontend/src/Components/CategoryGroup.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Category } from './Category'
+
+import '../style/CategoryGroup.css'
+
+export class CategoryGroup extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        categories: this.props.categories,
+        name: this.props.name
+      }
+      console.log(this.state.categories)
+    }
+
+    render() {
+      return (
+          <div>
+            <div className="categoryGroupTitle"> {this.state.name}
+            {this.state.categories.map(cat => {
+                return <Category category={cat} />
+            })}
+            </div>
+        </div>
+
+      );
+    }
+}
