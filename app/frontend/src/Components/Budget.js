9a84ac312a39b767c5673fe830c8af621faa1c44
Jeremy Lumley <jeremy.lumley96@gmail.com>
2022-01-22 23:50:07 -0500

    fixing budget scrolling

--- app/frontend/src/Components/Budget.js
+++ app/frontend/src/Components/Budget.js
@@ -8,67 +8,65 @@ import categoryRequests from '../requests/category';
 import '../style/Budget.css'
 
 export class Budget extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        screen_size: changeScreenSize(),
-        categories: {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      screen_size: changeScreenSize(),
+      categories: {}
+    }
+  }
+
+  groupCategories(categories) {
+    // Group categories by category group
+    var grouped_categories = {};
+    categories.map((c) => {
+      if (grouped_categories[c.category_group]) {
+        grouped_categories[c.category_group].push(c);
+      } else {
+        grouped_categories[c.category_group] = [];
       }
-    }
+    });
+    return grouped_categories;
+  };
 
-    groupCategories(categories){
-      // Group categories by category group
-      var grouped_categories = {};
-      categories.map((c)=>{
-        if (grouped_categories[c.category_group]){
-          grouped_categories[c.category_group].push(c);
-        } else {
-          grouped_categories[c.category_group] = [];
-        }
+  componentDidMount() {
+    this.fetchData()
+    window.addEventListener('resize', () => {
+      this.setState({ screen_size: changeScreenSize() });
+    });
+  }
+
+  fetchData() {
+    // get today's date YYYY-MM-DD
+    const today = new Date().toISOString().slice(0, 10);
+    instance.get(`${categoryRequests.fetchAllCategories}/${today}`).then(c => {
+      var grouped_categories = this.groupCategories(c.data);
+      console.log(grouped_categories)
+      this.setState({
+        categories: grouped_categories,
       });
-      return grouped_categories;
-    };
-
-    componentDidMount(){
-      this.fetchData()
-      window.addEventListener('resize', ()=>{
-        this.setState({screen_size: changeScreenSize()});
-      });
-    }
-
-    fetchData(){
-      // get today's date YYYY-MM-DD
-      const today = new Date().toISOString().slice(0, 10);
-      instance.get(`${categoryRequests.fetchAllCategories}/${today}`).then(c => {
-        var grouped_categories = this.groupCategories(c.data);
-        console.log(grouped_categories)
-        this.setState({
-            categories: grouped_categories,
-        });
-      });
-    };
+    });
+  };
 
 
 
-    render() {
-      return (
-        <div>
-          <div className="budgetContent">
-          <div className="budgetHeader"/>
-          <div className="budgetColumnNameGroup">
-              <div className={`budgetColumnNameCategory ${this.state.screen_size}BudgetColumnNameCategory`}>Category</div>
-              <div className={`budgetColumnName ${this.state.screen_size}BudgetColumnName`}>Assigned</div>
-              {(this.state.screen_size === "largeScreen") && (<div className={`budgetColumnName ${this.state.screen_size}BudgetColumnName`}>Spent</div>)}
-              <div className={`budgetColumnName ${this.state.screen_size}BudgetColumnName`}>Balance</div>
-          </div>
-          <div className="budgetCategories">
+  render() {
+    return (
+      <div className="budgetContent">
+        <div className="budgetHeader" />
+        <div className="budgetColumnTitles">
+          <div className={`budgetColumnNameCategory ${this.state.screen_size}BudgetColumnNameCategory`}>Category</div>
+          <div className={`budgetColumnName ${this.state.screen_size}BudgetColumnName`}>Assigned</div>
+          {(this.state.screen_size === "largeScreen") && (<div className={`budgetColumnName ${this.state.screen_size}BudgetColumnName`}>Spent</div>)}
+          <div className={`budgetColumnName ${this.state.screen_size}BudgetColumnName`}>Balance</div>
+        </div>
+        <div className="budgetCategories">
           {(Object.keys(this.state.categories).map((group) => {
-              return <CategoryGroup name={group} categories={this.state.categories[group]} />
+            return <CategoryGroup name={group} categories={this.state.categories[group]} />
           }))}
-          </div>
-         </div>
         </div>
 
-      );
-    }
+      </div>
+    );
+  }
 }
--- app/frontend/src/Components/CategoryGroup.js
+++ app/frontend/src/Components/CategoryGroup.js
@@ -4,25 +4,25 @@ import { Category } from './Category'
 import '../style/CategoryGroup.css'
 
 export class CategoryGroup extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        categories: this.props.categories,
-        name: this.props.name
-      }
-      console.log(this.state.categories)
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: this.props.categories,
+      name: this.props.name
     }
+    console.log(this.state.categories)
+  }
 
-    render() {
-      return (
-          <div>
-            <div className="categoryGroupTitle"> {this.state.name}
-            {this.state.categories.map(cat => {
-                return <Category category={cat} />
-            })}
-            </div>
-        </div>
+  render() {
+    return (
+      <div>
+        <div className="categoryGroupTitle"> {this.state.name} </div>
+        {this.state.categories.map(cat => {
+          return <Category category={cat} />
+        })}
 
-      );
-    }
+      </div>
+
+    );
+  }
 }
