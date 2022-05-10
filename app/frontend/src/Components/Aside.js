ee764ebd8117f07d4cb974389a84866c5843cb72
Jeremy Lumley <jeremy.lumley96@gmail.com>
2022-05-09 22:43:22 -0400

    getting started with react contexts

--- app/frontend/src/Components/Aside.js
+++ app/frontend/src/Components/Aside.js
@@ -1,54 +1,38 @@
-import React from 'react';
-import instance from '../axois';
-import accountRequests from '../requests/account';
+import React, { useContext } from 'react';
+import { AccountsContext } from './Layout';
 import { centsToMoney } from '../utils/money_utils';
 
 import { Link } from 'react-router-dom';
 
 import '../style/Aside.css'
 
-class Aside extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      accounts: [],
-    }
-  }
+export function Aside({ }) {
 
-  componentDidMount() {
-    this.fetchData();
-  };
+  const accountsContext = useContext(AccountsContext);
 
-  fetchData() {
-    instance.get(`${accountRequests.fetchAllAccounts}`).then((r) => {
-      this.setState({ accounts: r.data });
-    });
-  }
-
-  render() {
-    return (
-      <nav className="sidebar">
-        <Link className="sidebarLink" to="/"><div className="sidebarLinkDiv">Budget</div></Link>
-        <Link className="sidebarLink" to="/account/all"><div className="sidebarLinkDiv">All Accounts</div></Link>
-        {
-          this.state.accounts.map(a => {
-            return (
-              <Link className="sidebarLink" to={`/account/${a.id}`}>
-                <div className="sidebarLinkDiv">
-                  <div className="accountNameDiv">
-                    {a.name}
-                  </div>
-                  <div className="accountBalanceDiv">
-                    {centsToMoney(a.cleared_balance + a.uncleared_balance)}
-                  </div>
+  return (
+    <nav className="sidebar">
+      <Link className="sidebarLink" to="/"><div className="sidebarLinkDiv">Budget</div></Link>
+      <Link className="sidebarLink" to="/account/all"><div className="sidebarLinkDiv">All Accounts</div></Link>
+      {
+        accountsContext.accounts.map(a => {
+          return (
+            <Link className="sidebarLink" to={`/account/${a.id}`}>
+              <div className="sidebarLinkDiv">
+                <div className="accountNameDiv">
+                  {a.name}
                 </div>
-              </Link>);
-          })
-        }
-        <Link className="sidebarLink" to="/reports"><div className="sidebarLinkDiv">Reports</div></Link>
-      </nav>
-    );
-  }
+                <div className="accountBalanceDiv">
+                  {centsToMoney(a.cleared_balance + a.uncleared_balance)}
+                </div>
+              </div>
+            </Link>);
+        })
+      }
+      <Link className="sidebarLink" to="/reports"><div className="sidebarLinkDiv">Reports</div></Link>
+    </nav>
+  );
+
 }
 
 export default Aside;
--- app/frontend/src/Components/Layout.js
+++ app/frontend/src/Components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Aside from './Aside'
 import {
     Routes,
@@ -10,58 +10,60 @@ import { Account } from './Account';
 import Reports from './Reports';
 import { Header } from './Header';
 
+import instance from '../axois';
+import accountRequests from '../requests/account';
+
 import '../style/Layout.css'
 
-export const smallScreen = 1000;
+const SMALL_SCREEN = 1000;
+export const AccountsContext = React.createContext({
+    accounts: [],
+    setAccounts: () => { }
+});
 
-export class Layout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.toggleSidebar = this.toggleSidebar.bind(this)
-        this.state = {
-            smallScreen: setScreenSize(),
-            sidebar: false,
+function setScreenSize() {
+    if (window.innerWidth > SMALL_SCREEN) {
+        return false;
+    } else {
+        return true;
+    }
+}
+export function Layout() {
+    const [smallScreen, setSmallScreen] = useState(setScreenSize());
+    const [sidebar, setSidebar] = useState(false);
+    const [accounts, setAccounts] = useState([]);
+
+    function fetchAllAccounts() {
+        async function _fetchAllAccounts() {
+            setAccounts(await (await instance.get(accountRequests.fetchAllAccounts)).data)
         }
+        _fetchAllAccounts()
     }
 
-    toggleSidebar() {
-        this.setState({ sidebar: !this.state.sidebar });
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         window.addEventListener('resize', () => {
-            this.setState({
-                smallScreen: setScreenSize(),
-            });
-        });
-    }
+            setSmallScreen(setScreenSize());
+        })
+        fetchAllAccounts()
+    }, [])
 
-
-    render() {
-        return (
-            <div className="layout">
-                <Header smallScreen={this.state.smallScreen} toggleSidebar={this.toggleSidebar} />
+    return (
+        <div className="layout">
+            <Header smallScreen={smallScreen} toggleSidebar={() => { setSidebar(!sidebar) }} />
+            <AccountsContext.Provider value={{ accounts, setAccounts }}>
                 <div className="mainContent">
-                    {(this.state.sidebar || !this.state.smallScreen) && (
-                        <Aside sidebar={this.state.sidebar} />
+                    {(sidebar || !smallScreen) && (
+                        <Aside sidebar={sidebar} />
                     )}
                     <div className="workspaceContent">
                         <Routes>
                             <Route path="/account/:id" element={<Account />} />
                             <Route path="/reports" element={<Reports />} />
-                            <Route path="/" element={<Budget smallScreen={this.state.smallScreen} />} />
+                            <Route path="/" element={<Budget smallScreen={smallScreen} />} />
                         </Routes>
                     </div>
                 </div>
-            </div>
-        );
-    }
-}
-
-function setScreenSize() {
-    if (window.innerWidth > smallScreen) {
-        return false;
-    } else {
-        return true;
-    }
+            </AccountsContext.Provider>
+        </div>
+    );
 }
